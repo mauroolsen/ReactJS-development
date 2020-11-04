df044c61abb4fe6fe1cc7e51a453fb81e3de9668
mauroolsen <mauro123olsen@gmail.com>
2020-11-04 18:57:08 -0300

    sass

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from '@testing-library/react';
+import './App.scss';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );}
 }
 
 export default App;
