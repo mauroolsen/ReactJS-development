758eb76f8a186869a0ef51baca533f97a14b8dfc
mauroolsen <mauro123olsen@gmail.com>
2020-11-11 18:33:49 -0300

    Jugando con estilos

--- my-app/src/Components/HelloWorld.js
+++ my-app/src/Components/HelloWorld.js
@@ -14,25 +14,25 @@ class HelloWorld extends React.Component{
   }
 
   cambiarIdioma = () => {
-    this.setState((this.state.idioma == 1) ? {idioma:0} : {idioma:1}); 
+    this.setState((this.state.idioma === 1) ? {idioma:0} : {idioma:1}); 
   }
 
   render(){
     let response = null;
-    if(this.state.idioma == 1){
+    if(this.state.idioma === 1){
       response = (
         <div className="hello">
-          <h1>{this.idiomas.español}</h1>
-          <h4>{this.props.subtitle}</h4>
-          <button onClick={this.cambiarIdioma}>Cambiar idioma</button>
+          <h1 className="text_center">{this.idiomas.español}</h1>
+          <h4 className="text_center">{this.props.subtitle}</h4>
+          <button className="btn" onClick={this.cambiarIdioma}>Cambiar idioma</button>
         </div>
       );
     }else{
       response = (
         <div className="hello">
-          <h1>{this.idiomas.english}</h1>
-          <h4>{this.props.subtitle}</h4>
-          <button onClick={this.cambiarIdioma}>Cambiar idioma</button>
+          <h1 className="text_center">{this.idiomas.english}</h1>
+          <h4 className="text_center">{this.props.subtitle}</h4>
+          <button className="btn" onClick={this.cambiarIdioma}>Cambiar idioma</button>
         </div>
       );
     }
