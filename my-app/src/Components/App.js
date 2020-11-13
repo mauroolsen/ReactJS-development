b29614c389700e736d1e8418306e559ea56d4f83
mauroolsen <mauro123olsen@gmail.com>
2020-11-13 01:21:51 -0300

    React forms

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Task from './Task';
+import Form from './Form';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Form />
         <Task title="titulo" text="body"/>
       </div>
     );
--- my-app/src/Components/Task.js
+++ my-app/src/Components/Task.js
@@ -3,7 +3,6 @@ import React from 'react';
 class Task extends React.Component{
   constructor(props){
     super(props);
-    
   }
   render(){
     return(
