d009eb9f6b8e2edffe6aa5d2077006ae05dbc43f
Mauro OLSEN <mauro123olsen@gmail.com>
2020-11-14 17:11:24 -0300

    addTask function

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -2,16 +2,28 @@ import React from "react";
 
 import Tasks from './Tasks';
 import tasks from './sample/tasks.json';
+
+import Form from './Form';
 class App extends React.Component {
 
   state = {
     tasks: tasks
   };
 
+  addTask(task){
+    this.state.tasks.push(task);
+  }
+
   render() {
     return (
       <div className="App">
+        <Form/>
         <Tasks tasks = {this.state.tasks} />
+        {this.addTask({
+          "id": 4,
+          "title": "tarea tres",
+          "description": "description"
+        })}
       </div>
     );
   }
---
+++ my-app/src/Components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Form extends React.Component{
+    render(){
+        return(
+            <h1>
+                Formulario
+            </h1>
+        );
+    }
+}
+
+export default Form;
